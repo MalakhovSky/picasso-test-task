97371f307916e7a54491019c1a0fcfe210b5ed15
MalakhovSky <tviler6@gmail.com>
2023-12-01 22:34:11 +0300

    add postApi

---
+++ src/Pages/Posts/postsApi.ts
@@ -0,0 +1,31 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+export interface IPost{
+  userId?:number,
+  id?:number,
+  title?:string,
+  body:string
+}
+
+export const postApi=createApi({
+  reducerPath:'post',
+  baseQuery:fetchBaseQuery({baseUrl:'https://jsonplaceholder.typicode.com'}),
+  endpoints:(build)=>({
+    fetchAllPosts: build.query<IPost[],{limit:number,start:number}>({
+      query:({limit=5, start=0 })=>({
+        url:'/posts',
+        params:
+            {
+              _limit:limit,
+              _start:start,
+            }
+      })
+    }),
+    fetchPostById: build.query<IPost,number>({
+      query:(id:number)=>({
+        url:`/posts/${id}`,
+      })
+    })
+  })
+})
+
